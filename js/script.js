0d2bdd0a24a36bf78ed3df41233fba35fe85a4ff
Tiago Baião <tiago.baiao@springernature.com>
2026-01-16 19:56:54 +0000

    Transition to website rather than showing a letter inside the envelope

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.getElementById('envelope').addEventListener('click', function() {
-    this.classList.toggle('open');
+    this.classList.add('open');
+    setTimeout(() => {
+        this.style.transition = 'opacity 0.5s ease';
+        this.style.opacity = '0';
+        setTimeout(() => {
+            document.body.style.background = 'white';
+            document.getElementById('content').style.display = 'block';
+            this.style.display = 'none';
+        }, 500);
+    }, 800);
 });
